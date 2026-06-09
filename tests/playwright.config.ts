3f9af02c54f51886f1ff1c9e1d0edf095e2387cd
ArtoriasCode <artorias.code@gmail.com>
2026-06-09 20:17:42 +0300

    Add new tests

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -12,28 +12,46 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
   reporter: "html",
   use: {
     baseURL: process.env.BASE_URL ?? "https://127.0.0.1",
     ignoreHTTPSErrors: true,
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    trace: "retain-on-failure",
+    trace: "retain-on-failure"
   },
   projects: [
     {
       name: "setup",
-      testMatch: "**/auth.setup.ts",
+      testMatch: "**/auth.setup.ts"
     },
     {
-      name: "chromium",
+      name: "main",
       use: {
         ...devices["Desktop Chrome"],
         storageState: ".auth/session.json",
       },
       dependencies: ["setup"],
-      testIgnore: "**/logout.spec.ts",
+      testIgnore: ["**/logout.spec.ts", "**/servers.spec.ts", "**/server.spec.ts"]
+    },
+    {
+      name: "servers",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: ".auth/session.json",
+      },
+      dependencies: ["main"],
+      testMatch: "**/servers.spec.ts"
+    },
+    {
+      name: "server",
+      use: {
+        ...devices["Desktop Chrome"],
+        storageState: ".auth/session.json",
+      },
+      dependencies: ["servers"],
+      testMatch: "**/server.spec.ts"
     },
     {
       name: "logout",
@@ -41,8 +59,8 @@ export default defineConfig({
         ...devices["Desktop Chrome"],
         storageState: ".auth/session.json",
       },
-      dependencies: ["chromium"],
-      testMatch: "**/logout.spec.ts",
+      dependencies: ["server"],
+      testMatch: "**/logout.spec.ts"
     }
   ]
 })
