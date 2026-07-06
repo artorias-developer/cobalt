401066f0b53866afe889cdf8ba52cb30e8252e4e
ArtoriasCode <artorias.code@gmail.com>
2026-07-06 11:05:47 +0300

    Fix the WebSocket issue

--- cobalt/frontend/src/bootstrap.ts
+++ cobalt/frontend/src/bootstrap.ts
@@ -184,7 +184,7 @@ function setupProviders(app: App, router: ReturnType<typeof setupRouterInstance>
   app.provide(WS_LOGS_API_SERVICE_KEY, wsLogsApiService)
   app.provide(WS_SERVERS_API_SERVICE_KEY, wsServersApiService)
 
-  wsClient.connect(`wss://${window.location.hostname}/api/v1/ws`)
+  wsClient.connect(`wss://${window.location.host}/api/v1/ws`)
 
   return { httpUsersApiService, localeHelper, wsClient }
 }
